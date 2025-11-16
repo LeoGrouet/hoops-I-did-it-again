75fdf98110cb74ac24c87df98be2e0314b5b722d
Leo <leo.grouet@gmail.com>
2025-11-16 21:46:34 +0100

    feat: navbar customisation

--- hoops-esc/app/_layout.tsx
+++ hoops-esc/app/_layout.tsx
@@ -1,6 +1,24 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Stack, Tabs } from "expo-router";
+import { Tabs } from "expo-router";
+import Entypo from '@expo/vector-icons/Entypo';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 export default function RootLayout() {
-  return <Tabs />
+  return <Tabs>
+    <Tabs.Screen name="index"
+      options={{
+        title: "Home",
+        headerShown: true,
+        tabBarActiveTintColor: '#e91e63',
+        tabBarIcon: () => <Entypo name="home" size={24} color="black" />
+      }}
+    />
+    <Tabs.Screen name="homeGames"
+      options={{
+        title: "Games",
+        headerShown: true,
+        tabBarActiveTintColor: '#e91e63',
+        tabBarIcon: () => <FontAwesome6 name="basketball" size={24} color="black" />
+      }}
+    />
+  </Tabs>
 }
